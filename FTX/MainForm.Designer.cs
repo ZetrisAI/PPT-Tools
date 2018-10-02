cf17d9c21883783c0b34d9fffe446e48df2afa27
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-10-02 09:24:06 +0200

    Initial push

---
+++ FTX/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FTX {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelP1 = new System.Windows.Forms.Label();
+            this.labelP2 = new System.Windows.Forms.Label();
+            this.labelDelimiter = new System.Windows.Forms.Label();
+            this.ScanTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelP1
+            // 
+            this.labelP1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelP1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labelP1.Location = new System.Drawing.Point(45, 11);
+            this.labelP1.Name = "labelP1";
+            this.labelP1.Size = new System.Drawing.Size(45, 26);
+            this.labelP1.TabIndex = 0;
+            this.labelP1.Text = "0";
+            this.labelP1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelP2
+            // 
+            this.labelP2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelP2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labelP2.Location = new System.Drawing.Point(103, 11);
+            this.labelP2.Name = "labelP2";
+            this.labelP2.Size = new System.Drawing.Size(46, 26);
+            this.labelP2.TabIndex = 0;
+            this.labelP2.Text = "0";
+            // 
+            // labelDelimiter
+            // 
+            this.labelDelimiter.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDelimiter.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labelDelimiter.Location = new System.Drawing.Point(67, 11);
+            this.labelDelimiter.Name = "labelDelimiter";
+            this.labelDelimiter.Size = new System.Drawing.Size(59, 26);
+            this.labelDelimiter.TabIndex = 0;
+            this.labelDelimiter.Text = "-";
+            this.labelDelimiter.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ScanTimer
+            // 
+            this.ScanTimer.Enabled = true;
+            this.ScanTimer.Interval = 200;
+            this.ScanTimer.Tick += new System.EventHandler(this.ScanTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.ClientSize = new System.Drawing.Size(191, 49);
+            this.Controls.Add(this.labelP2);
+            this.Controls.Add(this.labelP1);
+            this.Controls.Add(this.labelDelimiter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "FTX";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelP1;
+        private System.Windows.Forms.Label labelP2;
+        private System.Windows.Forms.Label labelDelimiter;
+        private System.Windows.Forms.Timer ScanTimer;
+    }
+}
+
---
+++ FTX/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace FTX {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        static VAMemory PPT;
+
+        private enum MatchState {
+            Menu,
+            Match,
+            Finished
+        }
+
+        static MatchState match = MatchState.Finished;
+
+        int[] score = new int[2] {0, 0};
+        int[] total = new int[2] {0, 0};
+
+        private void ScanTimer_Tick(object sender, EventArgs e) {
+            if (PPT == null) {
+                if (Process.GetProcessesByName("puyopuyotetris").Length != 0) {
+                    PPT = new VAMemory("puyopuyotetris");
+                } else {
+                    return;
+                }
+            } else if (Process.GetProcessesByName("puyopuyotetris").Length == 0) {
+                PPT = null;
+                return;
+            }
+
+            int scoreAddress = PPT.ReadInt32(new IntPtr(0x14057F048));
+
+            if (scoreAddress == 0x0) {
+                if (match == MatchState.Finished) {
+                    match = MatchState.Menu;
+                    score[0] = score[1] = total[0] = total[1] = 0;
+                }
+            } else {
+                if (match == MatchState.Menu) {
+                    match = MatchState.Match;
+                }
+                scoreAddress += 0x38;
+            }
+
+            for (int i = 0; i < 2; i++) {
+                int temp = PPT.ReadInt32(new IntPtr(scoreAddress) + i * 4);
+                if (temp > score[i]) {
+                    total[i]++;
+                }
+                score[i] = temp;
+            }
+
+            if (PPT.ReadInt32(new IntPtr(0x140573A78)) != 0x0) {
+                if (match == MatchState.Match) {
+                    match = MatchState.Finished;
+                }
+            }
+
+            labelP1.Text = total[0].ToString();
+            labelP2.Text = total[1].ToString();
+        }
+    }
+}
