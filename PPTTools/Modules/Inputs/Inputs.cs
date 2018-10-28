9ff75ff6f9b8898c3b5b9f9fae25177620ac2c85
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-10-28 15:14:44 +0100

    Remove useless usings

--- PPTTools/Modules/Inputs/Inputs.cs
+++ PPTTools/Modules/Inputs/Inputs.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-
-namespace PPTTools {
+﻿namespace PPTTools {
     namespace Modules {
         public class Inputs {
             private int[] keyStates = new int[7] {0, 0, 0, 0, 0, 0, 0};
